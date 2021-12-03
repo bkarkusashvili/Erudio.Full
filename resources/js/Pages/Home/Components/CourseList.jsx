6afe3369dce6b7b6dc64b2f2ac33143c02609373
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2021-12-04 01:02:36 +0400

    build

--- resources/js/Pages/Home/Components/CourseList.jsx
+++ resources/js/Pages/Home/Components/CourseList.jsx
@@ -11,21 +11,23 @@ export const CourseList = ({ title, list, isReverce = false }) => {
             'course-list': true,
             reverse: isReverce
         })}>
-            <div className="container wrap">
-                <div className="info">
-                    <h3 className="tp-header">{title}</h3>
-                    <div className="content">
-                        <h4 className="tp-header mb-36 small">{item['name_' + lang]}</h4>
-                        <p className="tp-text">{item['goal_' + lang]}</p>
-                        <Link href={route('course.single', item.id)} children={'ვრცლად'} />
+            {item && (
+                <div className="container wrap">
+                    <div className="info">
+                        <h3 className="tp-header">{title}</h3>
+                        <div className="content">
+                            <h4 className="tp-header mb-36 small">{item['name_' + lang]}</h4>
+                            <p className="tp-text">{item['goal_' + lang]}</p>
+                            <Link href={route('course.single', item.id)} children={'ვრცლად'} />
+                        </div>
+                    </div>
+                    <div className="media">
+                        <figure>
+                            <img src={`${base}/storage/${item.image}`} alt={item['name_' + lang]} />
+                        </figure>
                     </div>
                 </div>
-                <div className="media">
-                    <figure>
-                        <img src={`${base}/storage/${item.image}`} alt={item['name_' + lang]} />
-                    </figure>
-                </div>
-            </div>
+            )}
         </section>
     );
 };
