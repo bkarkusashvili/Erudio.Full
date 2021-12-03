e9354ec97cd7f63512afa1eee620770308ee187a
bkarkusashvili <beqa.qarqusashvili.20@gmail.com>
2021-12-03 13:07:40 +0400

    build

--- resources/js/Pages/Home/Components/Clients.jsx
+++ resources/js/Pages/Home/Components/Clients.jsx
@@ -1,37 +1,49 @@
 import React, { useState } from 'react';
 import { usePage } from '@inertiajs/inertia-react';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { getClassName } from '@/Helper';
 
 export const Clients = ({ list = [] }) => {
     const { lang, base } = usePage().props;
     const [active, setActive] = useState();
-    // list = [...list, ...list, ...list, ...list, ...list, ...list, ...list,];
+    const [swiper, setSwiper] = useState();
 
     return (
         <section className="clients-wrap">
             <div className="container">
                 <h3 className="tp-header small mb-33">რას ამბობენ კლიენტები ჩვენზე</h3>
-                {list.map(item => (
-                    <p key={item.id} className="tp-text" children={item['text_' + lang]} />
+                {list.map((item, index) => (
+                    <p
+                        key={item.id}
+                        className={getClassName({ active: active === index, "tp-text client-text": true })}
+                        children={item['text_' + lang]}
+                    />
                 ))}
-                <Swiper className="clients-slider" centeredSlides={true} loop={true} onSlideChange={(e) => {
-                    console.log(e);
-                }} slidesPerView={5}>
-                    {list.map(item => (
-                        <SwiperSlide key={item.id}>
+                <Swiper
+                    className="clients-slider"
+                    centeredSlides={true}
+                    onInit={slider => {
+                        setSwiper(slider);
+                        setActive(slider.activeIndex)
+                    }}
+                    onSlideChange={slider => setActive(slider.activeIndex)}
+                    slidesPerView={5}
+                >
+                    {list.map((item, index) => (
+                        <SwiperSlide key={item.id} onClick={() => swiper.slideTo(index)}>
                             <div className="slider-wrap">
                                 <img src={`${base}/storage/${item.image}`} alt={item['name_' + lang]} />
                             </div>
                         </SwiperSlide>
                     ))}
-                    {/* <div className="navigation">
-                        <a href="" className="left">
-                            <i className="icon icon-slide-arrow"></i>
+                    <div className="navigation">
+                        <a className="left" onClick={() => swiper.slidePrev()}>
+                            <i className="icon icon-slide-arrow-small"></i>
                         </a>
-                        <a href="" className="right">
-                            <i className="icon icon-slide-arrow icon-rotate-180"></i>
+                        <a className="right" onClick={() => swiper.slideNext()}>
+                            <i className="icon icon-slide-arrow-small icon-rotate-180"></i>
                         </a>
-                    </div> */}
+                    </div>
                 </Swiper>
             </div>
         </section>
