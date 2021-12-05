3269a32da18530842b1b644c4a1168b3e8cf78c6
Beka Karkusashvili <beqa.qarqusashvili.20@gmail.com>
2021-12-05 20:00:02 +0400

    build

---
+++ database/migrations/2021_12_05_153731_add_days_to_courses.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddDaysToCourses extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->smallInteger('days')->default(0)->after('text_en');
+            $table->smallInteger('type')->default(0)->after('days');
+            $table->string('url')->nullable()->after('type');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('courses', function (Blueprint $table) {
+            $table->dropColumn('days');
+            $table->dropColumn('type');
+            $table->dropColumn('url');
+        });
+    }
+}
