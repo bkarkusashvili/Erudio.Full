6c1123aba1179269804fdd35f479dbb2a724db86
Beka Karkusashvili <beqa.qarqusashvili.20@gmail.com>
2021-12-21 23:32:55 +0400

    bss

--- app/Notifications/SubscribeNotification.php
+++ app/Notifications/SubscribeNotification.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class SubscribeNotification extends Notification
+class ChangeEmailNotification extends Notification
 {
     use Queueable;
 
